45156b064148a69238eaa0e02640ea92ae94ddc6
ag-golmodamas <a.golmodamas@gmail.com>
2026-07-28 08:59:07 +0330

    fix: PWA now working in dev mode

--- frontend/my-spotify/public/sw.js
+++ frontend/my-spotify/public/sw.js
@@ -1,27 +1,7 @@
-const CACHE_NAME = "my-spotify-v1";
-
-self.addEventListener("install", (event) => {
+self.addEventListener("install", () => {
   self.skipWaiting();
-
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll([
-        "/",
-      ]);
-    })
-  );
 });
 
 self.addEventListener("activate", (event) => {
   event.waitUntil(self.clients.claim());
-});
-
-self.addEventListener("fetch", (event) => {
-  if (event.request.method !== "GET") return;
-
-  event.respondWith(
-    caches.match(event.request).then((cachedResponse) => {
-      return cachedResponse || fetch(event.request);
-    })
-  );
 });
